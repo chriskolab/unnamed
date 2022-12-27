3aee37c72f667a63eea5f8e43734fffc282b023a
Christoph <82673785+chriskolab@users.noreply.github.com>
2022-12-27 19:56:58 +0100

    Add files via upload

--- script/game_res.js
+++ script/game_res.js
@@ -9,25 +9,37 @@ function calcFinalStanding() {
 
 	//Get data from web storage
 	var gameMode = localStorage.getItem("game-mode");
+	var strPlayerPoints = localStorage.getItem("player-points");
 
 	//Str2Num
 	gameMode = Number(gameMode);
 
 	switch(gameMode) {
-	case 0: case 1: case 2: case 3:
-		sortByPlayerPoints();
-		//Already implemented function, where round number is not needed
-		//Open TODO: find good name for function
-		//IDEA: createTable(numberOfColumns) + sortByPlayerPoints() + fillTable()
-		break;
+	case 0: case 1: case 2: case 3: case 4: case 5: case 6: case 7:
 
-	case 4: case 5: case 6: case 7:
-		sortByPlayerPoints();
-		//New, also needed function
-		//Open TODO: save round number, when each player reaches final points
-		//IDEA: createTable(numberOfColumns) + sortByGameRound() + fillTable()
+		//Get number of rows for result table
+		const arrPlayerPoints = strPlayerPoints.split(",").map(Number);
+		var numRows = new Set(arrPlayerPoints).size;
+
+		createTable(numRows,3);
+		sortedArray = sortByPlayerPoints(numRows);
+
 		break;
+/*
+	case 4: case 5: case 6: case 7:
+
+		//Get number of rows for result table???
+		const arrPlayerRounds = strPlayerRounds.split(",").map(Number);
+		var numRows = new Set(arrPlayerRounds).size;
+
+		createTable(numRows,4);
+		sortedArray = sortByGameRound(gameMode,numRows);
+
+		break;
+*/
 	}
+
+	fillTable(sortedArray);
 }
 
 
@@ -37,25 +49,33 @@ function sortByPlayerPoints() {
 
 	//Get data from web storage
 	var gameMode = localStorage.getItem("game-mode");
-	var numPlayers = localStorage.getItem("num-players");
-	var strPlayerNames = localStorage.getItem("player-names");
 	var strPlayerPoints = localStorage.getItem("player-points");
+	var strPlayerNames = localStorage.getItem("player-names");
 
 	//Str2Num
 	gameMode = Number(gameMode);
-	numPlayers = Number(numPlayers);
 
-	//Convert player names and points
-	const arrPlayerPoints = strPlayerPoints.split(",").map(Number);         //str2arr of numbers
-	const arrPlayerNames = strPlayerNames.split(",");												//str2arr
-	var playerName;
-	var playerPoints;
+	//Declare empty arrays for columns
+	var arrPosition = [];
+	var arrPlayers = [];
+	var arrPoints = [];
 
-	//Get sorted indeces form player points
-	indexedArr = arrPlayerPoints.map(function(e,i){return {ind: i, val: e}});									//make list with indices and values
-	indexedArr.sort(function(x, y){return x.val > y.val ? 1 : x.val == y.val ? 0 : -1});			//sort index/value couples, based on values
+	//Fill array for positions
+	for (var i = 0; i < numRows; i++) {
+		arrPosition[i] = i+1 + ".";
+	}
+
+	//Remove duplicates in player points by temporary creating a set
+	var arrPlayerPoints = strPlayerPoints.split(",").map(Number);         //str2arr of numbers
+	var setPlayerPoints = new Set(arrPlayerPoints);				//arr2set
+	arrPlayerPointsShort = Array.from(setPlayerPoints);				//set2arr
+
+	//Get sorted indeces from player points
+	indexedArr = arrPlayerPointsShort.map(function(e,i){return {ind: i, val: e}});				//Create array with indices and values
+	indexedArr.sort(function(x, y){return x.val > y.val ? 1 : x.val == y.val ? 0 : -1});			//Sort index/value couples, based on values
 	
-	switch(gameMode) {																				//make list keeping only indices and reverse order depending on game mode
+	//Make list keeping only indices and reverse order depending on game mode
+	switch(gameMode) {
 	case 0: case 1: case 4: case 5:
 		indices = indexedArr.map(function(e){return e.ind}).reverse();
 		break;
@@ -64,155 +84,37 @@ function sortByPlayerPoints() {
 		indices = indexedArr.map(function(e){return e.ind});
 		break;
 	}
-	
-	//Create variables for final standing
-	var temp_points;																//Create temporary variable for players with same end points
-	var temp_placement = 1;													//Create temporary variable for placement table sizes
-	var temp_position = 1;													//Create temporary variable for positions
 
-	//Create table for game results
-	var tdiv = document.getElementById("final-standing");
-	const tbl = document.createElement("table");
-	const tbdy = document.createElement("tbody");
-	tbl.setAttribute("class", "leaderboard-endscreen");
+	//Fill array for player points
+	for (var i = 0; i < numRows; i++) {
+		arrPoints[i] = arrPlayerPointsShort[indices[i]];
+	}
 
-	var cellPosText;
-	var cellPlayerText;
-	var rowID = 0;
+	var arrPlayerNames = strPlayerNames.split(",");				//str2arr
 
-	//Iterate through sorted indices and create table
-	for(i=0;i<numPlayers;i++) {
+	//Fill array for player names
+	for (var i = 0; i < arrPlayerNames.length; i++) {
 
-		playerName = arrPlayerNames[indices[i]];
-		playerPoints = arrPlayerPoints[indices[i]];
+		for (var j = 0; j < arrPoints.length; j++) {
 
-		if (playerPoints!=temp_points) {
-			//console.log(playerPoints);
-			const row = document.createElement("tr");
-			const cellPos = document.createElement("td");
-			const cellPlayer = document.createElement("td");
-			const cellPoints = document.createElement("td");
-			cellPos.style.width = "40px";
-			cellPlayer.style.width = "auto";
-			cellPoints.style.width = "auto";
+			var playerPoints = arrPlayerPoints[i];
+			var sortedPoints = arrPoints[j];
 
-			cellPlayer.setAttribute("id", "row-" + rowID);
-			rowID++;
-
-			switch(temp_placement) {
-			case 1:
-				size = "25px";
-
-				cellPosText = document.createTextNode(temp_position + ".");
-				cellPos.style.fontSize = size;
-				cellPlayer.style.fontSize = size;
-				cellPoints.style.fontSize = size;
-				cellPos.appendChild(cellPosText);
-				row.appendChild(cellPos);
-
-				temp_placement++;
-				temp_position++;
-				break;
-
-			case 2:
-				size = "20px";
-
-				cellPosText = document.createTextNode(temp_position + ".");
-				cellPos.style.fontSize = size;
-				cellPlayer.style.fontSize = size;
-				cellPoints.style.fontSize = size;
-				cellPos.appendChild(cellPosText);
-				row.appendChild(cellPos);
-
-				temp_placement++;
-				temp_position++;
-				break;
-
-			case 3:
-				size = "15px";
-
-				cellPosText = document.createTextNode(temp_position + ".");
-				cellPos.style.fontSize = size;
-				cellPlayer.style.fontSize = size;
-				cellPoints.style.fontSize = size;
-				cellPos.appendChild(cellPosText);
-				row.appendChild(cellPos);
-
-				temp_placement++;
-				temp_position++;
-				break;
-
-			default:
-				size = "12px";
-
-				cellPosText = document.createTextNode(temp_position + ".");
-				cellPos.style.fontSize = size;
-				cellPlayer.style.fontSize = size;
-				cellPoints.style.fontSize = size;
-				cellPos.appendChild(cellPosText);
-				row.appendChild(cellPos);
-
-				temp_position++;
+			if (playerPoints == sortedPoints) {
+				arrPlayers[j] += arrPlayerNames[i] + "<br>";
+				
 			}
-
-			tbdy.appendChild(row);
-
-			row.appendChild(cellPlayer);
-		    tbdy.appendChild(row);
-
-			const cellText = document.createTextNode(playerPoints);
-			cellPoints.appendChild(cellText);
-			row.appendChild(cellPoints);
-			tbdy.appendChild(row);
-
-		}		
-
-		//console.log(playerName);
-
-		temp_points = playerPoints;
-
+		}
 	}
 
-	tbl.appendChild(tbdy);
-	tdiv.appendChild(tbl);
-
-	//console.log("-------------");
-
-	//Insert player names in table rows
-	var temp_points;
-	var temp_switch = 0;
-	var rowID = -1;
-	var allPlayerNames;
-
-	for(i=0;i<numPlayers;i++) {
-
-		playerName = arrPlayerNames[indices[i]];
-		playerPoints = arrPlayerPoints[indices[i]];
-
-		if (playerPoints!=temp_points) {
-			allPlayerNames = playerName;
-			temp_switch = 1;
-
-		} else {
-			allPlayerNames += "<br>" + playerName;
-
-		}
-
-		if (temp_switch) {
-			rowID++;
-			temp_switch = 0;
-		}
-
-		console.log(allPlayerNames);
-		console.log(temp_switch);
-		console.log(i);
-
-		document.getElementById("row-" + rowID).innerHTML = allPlayerNames;
-
-		temp_points = playerPoints;
-
+	//Remove string "undefinded" in every array element
+	for (var i = 0; i < arrPlayers.length; i++) {
+		arrPlayers[i] = arrPlayers[i].substring(9);
 	}
 
+	sortedArray = [arrPosition, arrPlayers, arrPoints]
+
+	return sortedArray;
 }
 
 
@@ -220,4 +122,92 @@ function sortByPlayerPoints() {
 //
 function sortByGameRound() {
 
+}
+
+
+
+//
+function createTable(numRows, numCols) {
+
+	var tdiv = document.getElementById("final-standing");
+	const tbl = document.createElement("table");
+
+	//Create column group for width of first column
+	const cg = document.createElement("colgroup");
+	const col = document.createElement("col");
+	col.setAttribute("span", "1");
+	col.setAttribute("width", "40px");
+	cg.appendChild(col);
+	tbl.appendChild(cg);
+
+	//Create empty table
+	const tbdy = document.createElement("tbody");
+
+	tbl.style.width = "100%";
+	tbl.setAttribute("border", "1");
+
+	for (var i = 0; i < numRows; i++) {
+
+		const row = document.createElement("tr");
+
+		for (var j = 0; j < numCols; j++) {
+
+			const cell = document.createElement("td");
+			const cellText = document.createTextNode("cell-" + i + j);
+			cell.setAttribute("id", "cell-" + i + j);
+			cell.style.fontSize = "10px";
+
+			cell.appendChild(cellText);
+			row.appendChild(cell);
+		}
+
+		tbdy.appendChild(row);
+	}
+
+	tbl.appendChild(tbdy);
+	tdiv.appendChild(tbl);
+}
+
+
+
+//
+function fillTable(items) {
+
+	//Get size of two dimensional input array
+	const [rows,cols] = [ items[0].length, items.length ];
+
+	//Temporary counter for row sizes
+	var tblRow = 1;
+
+	//Fill table with item values
+	for (var i = 0; i < rows; i++) {
+
+		//Set row sizes
+		switch(tblRow) {
+		case 1:
+			size = "25px";
+			tblRow++;
+			break;
+
+		case 2:
+			size = "20px";
+			tblRow++
+			break;
+
+		case 3:
+			size = "15px";
+			tblRow++
+			break;
+
+		default:
+			size = "12px";
+			break;
+		}
+
+		//Insert values in cells
+		for (var j = 0; j < cols; j++) {
+			document.getElementById("cell-" + i + j).innerHTML = items[j][i];
+			document.getElementById("cell-" + i + j).style.fontSize = size;
+		}
+	}
 }
